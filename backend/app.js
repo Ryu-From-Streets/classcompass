278543cb6f6c526fba22109f0e25aee63ed7cc7c
Pratim Sadhu <pratimsadhu@icloud.com>
2024-02-28 11:00:01 -0500

    Boilerplate Express

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+});
+
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log(
+            "Server is Successfully Running, and App is listening on port " +
+                PORT
+        );
+    else console.log("Error occurred, server can't start", error);
+});
