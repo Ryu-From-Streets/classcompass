c96373e71d8ba895ac2bcbb0542000755c75a208
Michael <46937389+LastSpot@users.noreply.github.com>
2024-03-18 11:42:50 -0400

    setup webscraper

---
+++ backend/WebScraper.java
@@ -0,0 +1,13 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.*;
+
+public class WebScraper {
+    public static void main(String[] args) throws IOException {
+
+
+
+    }
+}
