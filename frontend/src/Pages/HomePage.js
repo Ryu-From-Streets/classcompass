b70f754d2cc7056d24d4cf27f7118dca0a507953
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-03-24 21:41:33 -0400

    Created Home Page
    Created a home page that fetches all courses and displays each course's information.
    
    Course information is currently fetched from the test data in the Public directory since the backend API is not yet created. Once the backend API is created, this fetch will need to be updated to fetch the data from the backend API.
    
    Courses are currently displayed in plaintext. This will need to be changed to some kind of Course Node component which has yet to be created. I(Ryu) am thinking of using "react flow" for this but haven't gotten around to learning it and seeing its viability yet.

---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+    const [ courses, setCourses ] = useState(null);
+
+    useEffect(() => {
+        const fetchAllCourses = async () => {
+            // Currently fetching data from local files for test purposes
+            // TODO: integrate with backend(Need backend to set up their api first)
+            //       will probably be along the lines of fetch("http://localhost:PORT")
+            //       where PORT is whatever port we end up using for backend
+            const response = await fetch("./Test_Data/Courses.json");
+            const jsonOfCourses = await response.json();
+
+            if (response.ok) {
+                setCourses(jsonOfCourses);
+            }
+        }
+
+        fetchAllCourses();
+    }, []);
+    
+    return (
+        <div className="HomePage">
+            <div className="Courses">
+
+                {courses && courses.map((course) => (
+
+                    // Temporary visualization of course data
+                    // TODO: Create component for an individual course node specifically
+                    //       I was thinking of using "react flow" for this but haven't had
+                    //       the chance to learn it yet so doing this for now
+                    <div className="TMP-Course-Data">
+                        <h4>{course.Name}</h4>
+                        <p>Instructors: {course.Instructors}</p>
+                        <p>Description: {course.Description}</p>
+                        <p>Prerequisites: {course.Prerequisites}</p>
+                    </div>
+
+                ))}
+
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
