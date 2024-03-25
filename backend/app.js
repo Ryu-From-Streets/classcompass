a20275d90f611dee850d87e6ba3882fc2408c461
Pratim Sadhu <pratimsadhu@icloud.com>
2024-03-24 20:29:51 -0400

    Running the Class-Compass Server

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 
 const app = express();
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Welcome to the ClassCompass Development");
+    res.status(200).send("Welcome to the root URL of Server");
+});
+
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log(
+            `ClassCompass is successfully running, and the app is listening on port ${PORT}`
+        );
+    else console.log("Error occurred, server can't start", error);
 });
