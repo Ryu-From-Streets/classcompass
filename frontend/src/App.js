6397d3dcd72f873a9e84dee455c4526709c05e68
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-03-24 21:43:35 -0400

    Added routes and cleaned up formatting
    Cleaned up some formatting and removed template code.
    
    Added routes so that default path is routed to a homepage.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,46 @@
-import logo from './logo.svg';
-import { useState } from "react";
 import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+// PAGES
+import HomePage from "./Pages/HomePage";
+
+// COMPONENTS
 import { SearchBar } from './Components/SearchBar';
 import { SearchResultsList } from './Components/SearchResultList';
 
+
 function App() {
+  // state for search bar components
   const [results, setResults] = useState([]);
 
   return (
     <div className="App">
+
+
+
       <div className = "search-bar-container">
         <SearchBar setResults={setResults}/>
         <SearchResultsList results={results}/>
       </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <BrowserRouter>
+        <div className="Pages">
+          <Routes>
+
+            <Route 
+              path="/"
+              element={<HomePage />}
+            />
+
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
