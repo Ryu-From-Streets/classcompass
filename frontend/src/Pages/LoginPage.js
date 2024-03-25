ea682154015396cb96b3b4453ac021f1fb5d0eec
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-03-24 21:38:04 -0400

    Created Login and Register Pages
    Created login and register pages that are currently blank and not referenced anywhere yet.
    Just created them so we have it later on when we start adding user authentication.

---
+++ frontend/src/Pages/LoginPage.js
@@ -0,0 +1,11 @@
+
+
+const LoginPage = () => {
+    return (
+        <div className="LoginPage">
+            <h2>LOGIN PAGE</h2>
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ frontend/src/Pages/RegisterPage.js
@@ -0,0 +1,11 @@
+
+
+const RegisterPage = () => {
+    return (
+        <div className="RegisterPage">
+            <h2>Register PAGE</h2>
+        </div>
+    );
+}
+
+export default RegisterPage;
