3272167e1acd3bd5e786a41eb4270aec46ed781e
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-03-26 13:56:22 -0400

    Created NavBar Component
    Created a Navigation Bar component adapted from: https://www.codevertiser.com/reactjs-responsive-navbar/
    
    Only basic functionality for now and may need to be tweaked in the future.

---
+++ frontend/src/Components/NavigationBar.js
@@ -0,0 +1,32 @@
+import "./NavigationBar.css";
+
+import { NavLink } from "react-router-dom";
+
+/* Adapted from: https://www.codevertiser.com/reactjs-responsive-navbar/ */
+
+const NavigationBar = () => {
+    return (
+        <nav className="NavigationBar">
+            <div className="NavigationBarContainer">
+                <div className="Logo">
+                    <h1>Class Compass</h1>
+                </div>
+                <div className="NavigationElements">
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register">Register</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login">Login</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+ 
+export default NavigationBar;
