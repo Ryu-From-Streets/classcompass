502f7d1862e71f78a8c7d44f9a7daf4274fd1786
Michael <46937389+LastSpot@users.noreply.github.com>
2024-03-27 11:06:20 -0400

    clean up db file

--- backend/database.js
+++ backend/database.js
@@ -9,16 +9,6 @@ const COLLECTION_NAME = "courses";
 
 var client, db, collection;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri,  {
-//         serverApi: {
-//             version: ServerApiVersion.v1,
-//             strict: true,
-//             deprecationErrors: true,
-//         }
-//     }
-// );
-
 async function initialize() {
   try {
     client = new MongoClient(uri, {
@@ -40,20 +30,6 @@ async function initialize() {
     throw err;
   }
 }
-
-// async function run() {
-//   try {
-//     // Connect the client to the server (optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     const db = client.db(DATABASE_NAME);
-//     const collection = db.collection(COLLECTION_NAME);
-//     console.log("You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
 
 async function insertCourse() {
   try {
