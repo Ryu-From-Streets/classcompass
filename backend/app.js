a6498618e4b3fefbfc4a5ae3c3778b25fab07182
Pratim Sadhu <pratimsadhu@icloud.com>
2024-03-27 10:22:09 -0400

    Routing

--- backend/app.js
+++ backend/app.js
@@ -10,21 +10,21 @@ app.get("/api/courses", (req, res) => {
     res.send(courses);
 });
 
-app.route("/api/courses/:id")
-    .get("/api/courses/:id", (req, res) => {
-        const id = req.params.id;
-        const course = courses.find((course) => course.id === parseInt(id));
-        if (!course) {
-            return res
-                .status(404)
-                .send("The course with the given ID was not found.");
-        }
-        res.send(course);
-    })
-    .patch("/api/courses/:id", (req, res) => {})
-    .delete("/api/courses/:id", (req, res) => {});
+// app.route("/api/courses/:id")
+//     .get("/api/courses/:id", (req, res) => {
+//         const id = req.params.id;
+//         const course = courses.find((course) => course.id === parseInt(id));
+//         if (!course) {
+//             return res
+//                 .status(404)
+//                 .send("The course with the given ID was not found.");
+//         }
+//         res.send(course);
+//     })
+//     .patch("/api/courses/:id", (req, res) => {})
+//     .delete("/api/courses/:id", (req, res) => {});
 
-app.route("/api/courses").post((req, res) => {});
+// app.route("/api/courses").post((req, res) => {});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
