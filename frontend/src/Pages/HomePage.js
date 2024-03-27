57e35b90ca26989a57d56a7b32438ad3c546253b
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-03-26 21:07:48 -0400

    Updated Home page to use course buttons
    Updated home page to list each course as a course button instead of the temporary plaintext from before.

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 // COMPONENTS
 import { SearchBar } from '../Components/SearchBar';
+import CourseButton from "../Components/CourseButton"
 
 
 const HomePage = () => {
@@ -38,16 +39,7 @@ const HomePage = () => {
 
                 {filteredCourses && filteredCourses.map((course) => (
 
-                    // Temporary visualization of course data
-                    // TODO: Create component for an individual course node specifically
-                    //       I was thinking of using "react flow" for this but haven't had
-                    //       the chance to learn it yet so doing this for now
-                    <div className="TMP-Course-Data">
-                        <h4>{course.Name}</h4>
-                        <p>Instructors: {course.Instructors}</p>
-                        <p>Description: {course.Description}</p>
-                        <p>Prerequisites: {course.Prerequisites}</p>
-                    </div>
+                    <CourseButton course={course}/>
 
                 ))}
 
