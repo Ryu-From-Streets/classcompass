20eb577c162d8e57684aabe0f65743fe04574bb4
Pratim Sadhu <pratimsadhu@icloud.com>
2024-04-07 20:59:10 -0400

    Removed deprecated option for mongo

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectMongoDB(url) {
-    return mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    return mongoose.connect(url)
 }
 
 module.exports = { connectMongoDB };
