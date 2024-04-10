57455e81943a1d757a0fdbd538359247fd0993f9
Michael <46937389+LastSpot@users.noreply.github.com>
2024-04-10 13:28:22 -0400

    got prerequistite and credit

--- backend/webScapper.py
+++ backend/webScapper.py
@@ -1,11 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
+import pprint
 
 url = 'https://www.cics.umass.edu/content/fall-24-course-descriptions'
 response = requests.get(url)
 content = response.content
-
 soup = BeautifulSoup(content, 'html.parser')
 
-with open('course.txt', 'w', encoding='utf-8') as f:
-    f.write('%s\n' %soup)
+# Getting course titles from h2 with anchor with id in it
+courseTitles = [h2.a.get_text() for h2 in soup.find_all('h2') if h2.a]
+courseDescriptions = [p.get_text() for p in soup.find_all('p') if not p.find('a')]
+coursePrerequisite = []
+courseCredits = []
+
+for i in range(len(courseDescriptions) - 10):
+    info = courseDescriptions[i].split(':')[-1]
+    if 'Prerequisite' in courseDescriptions[i]:
+        for i in range(len(info) - 2, -1, -1):
+            if info[i] == '.':
+                prerequisite = info[:i].strip()
+                credit = info[i + 1:].strip()
+                break
+        coursePrerequisite.append(prerequisite)
+        courseCredits.append(credit)
+    else:
+        
+        for i in range(len(info) - 2, -1, -1):
+            if info[i] in '23456789':
+                credit = info[i : i + 9]
+                break
+            elif info[i] == '1':
+                credit = info[i : i + 8]
+                break
+        coursePrerequisite.append(None)
+        courseCredits.append(credit)
+
+print(coursePrerequisite)
+print(courseCredits)
