bf6b36524fe574a9a2b08989c60a39bfdc2c6509
terminal-lucidity <kfphalways@gmail.com>
2024-04-14 14:18:38 -0400

    Skeletal Login Page

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,10 +1,33 @@
+import React, {useState} from "react"
 
 
 const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleLogin = (e) => {
+        //auth logic from backend
+    };
     return (
-        <div className="LoginPage">
-            <h2>LOGIN PAGE</h2>
-        </div>
+      <div className="LoginPage">
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type = "submit" />
+        </form>
+      </div>
     );
 }
 
