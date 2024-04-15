a82fd1e2150c1b6ceb468a61f54332cba69f35d5
terminal-lucidity <kfphalways@gmail.com>
2024-04-15 19:37:40 -0400

    some changes to login

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -2,7 +2,7 @@ import React, {useState} from "react"
 import { useHistory } from "react-router-dom";
 
 
-const LoginPage = () => {
+const LoginPage = ({isShowLogin}) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const handleLogin = (e) => {
@@ -10,24 +10,41 @@ const LoginPage = () => {
     };
     return (
       <div className="LoginPage">
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <input
-            type="email"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type = "submit"> Login </button>
-        </form>
+        <div className="form-box solid">
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className="input-box">
+              <input
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>{" "}
+            <br></br>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="forgot">
+              <label>
+                <input type="checkbox" />
+                Remember me
+              </label>
+              <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit"> Login </button>
+            <div className="register">
+              <p>Don't have an account?</p> <a href="#">Register</a>
+            </div>
+          </form>
+        </div>
       </div>
     );
 }
