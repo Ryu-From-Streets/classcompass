72cb2d568c5053b2ed26f9889de42eb98faa6303
terminal-lucidity <kfphalways@gmail.com>
2024-04-15 20:27:36 -0400

    login is making progress!

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
-import { useHistory } from "react-router-dom";
-
+import { CiUser, CiLock } from "react-icons/ci";
+import "./login.css"
 
 const LoginPage = ({isShowLogin}) => {
     const [email, setEmail] = useState("");
@@ -9,42 +9,42 @@ const LoginPage = ({isShowLogin}) => {
        
     };
     return (
-      <div className="LoginPage">
-        <div className="form-box solid">
-          <h2>Login</h2>
-          <form onSubmit={handleLogin}>
-            <div className="input-box">
-              <input
-                type="email"
-                placeholder="Enter Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>{" "}
-            <br></br>
-            <div className="input-box">
-              <input
-                type="password"
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="forgot">
-              <label>
-                <input type="checkbox" />
-                Remember me
-              </label>
-              <a href="#">Forgot Password?</a>
-            </div>
-            <button type="submit"> Login </button>
-            <div className="register">
-              <p>Don't have an account?</p> <a href="#">Register</a>
-            </div>
-          </form>
-        </div>
+      <div className="login-page">
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="input-box">
+            <input
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <CiUser className="icon" />
+          </div>
+          <br></br>
+          <div className="input-box">
+            <input
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <CiLock className="icon" />
+          </div>
+          <div className="forgot">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button type="submit"> Login </button>
+          <div className="register">
+            <p>Don't have an account?</p> <a href="#">Register</a>
+          </div>
+        </form>
       </div>
     );
 }
