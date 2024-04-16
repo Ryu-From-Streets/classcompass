2e5659b54d42253c4378cf3ee849e9622b8247c2
terminal-lucidity <kfphalways@gmail.com>
2024-04-16 15:48:43 -0400

    Better UI

--- frontend/src/Components/NavigationBar.js
+++ frontend/src/Components/NavigationBar.js
@@ -1,32 +1,34 @@
 import "./NavigationBar.css";
 
 import { NavLink } from "react-router-dom";
+import { HiOutlineUserCircle } from "react-icons/hi";
 
 /* Adapted from: https://www.codevertiser.com/reactjs-responsive-navbar/ */
 
 const NavigationBar = () => {
-    return (
-        <nav className="NavigationBar">
-            <div className="NavigationBarContainer">
-                <div className="Logo">
-                    <h1>Class Compass</h1>
-                </div>
-                <div className="NavigationElements">
-                    <ul>
-                        <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/advisors">Advisors</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/login">Login</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="NavigationBar">
+      <div className="NavigationBarContainer">
+        <div className="Logo">
+          <NavLink to="/" exact>
+            <h1>Class Compass</h1>
+          </NavLink>
+        </div>
+        <div className="NavigationElements">
+          <ul>
+            <li>
+              <NavLink to="/advisors">Advisors</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">
+                <HiOutlineUserCircle className="icon" />
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
- 
+
 export default NavigationBar;
