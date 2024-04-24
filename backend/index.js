8ad94897e299b282cd33f25ae0efef0478d29960
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-04-24 16:03:42 -0400

    Removed python script on startup
    Removed python script on startup, as the data it gathered has already been placed into the database, until future functionality may* add automatic updating.

--- backend/index.js
+++ backend/index.js
@@ -2,53 +2,53 @@ const express = require("express");
 require("dotenv").config();
 
 const { connectMongoDB } = require("./connection");
-const { spawn } = require("child_process");
-// const { handleCreateCourse } = require("./controllers/course");
-// const Course = require("./models/course");
+// const { spawn } = require("child_process");
+// // const { handleCreateCourse } = require("./controllers/course");
+// // const Course = require("./models/course");
 
-/**
- * Run the Python web scrapper script
- * @returns None
- */
-function runScrapper() {
-    // Attempt to use 'python' first
-    let pythonProcess = spawn("python", ["./webScrapper.py"]);
+// /**
+//  * Run the Python web scrapper script
+//  * @returns None
+//  */
+// function runScrapper() {
+//     // Attempt to use 'python' first
+//     let pythonProcess = spawn("python", ["./webScrapper.py"]);
 
-    pythonProcess.on("error", (err) => {
-        if (err.code === "ENOENT") {
-            console.log("'python' not found, trying 'python3' instead...");
+//     pythonProcess.on("error", (err) => {
+//         if (err.code === "ENOENT") {
+//             console.log("'python' not found, trying 'python3' instead...");
 
-            // If 'python' is not found - try 'python3'
-            pythonProcess = spawn("python3", ["./webScrapper.py"]);
+//             // If 'python' is not found - try 'python3'
+//             pythonProcess = spawn("python3", ["./webScrapper.py"]);
 
-            setupProcessHandlers(pythonProcess);
+//             setupProcessHandlers(pythonProcess);
 
-            pythonProcess.on("error", (err) => {
-                if (err.code === "ENOENT") {
-                    console.error("Neither 'python' nor 'python3' could be found - \nPlease install Python first.");
-                    exit();
-                }
-            });
-        }
-    });
+//             pythonProcess.on("error", (err) => {
+//                 if (err.code === "ENOENT") {
+//                     console.error("Neither 'python' nor 'python3' could be found - \nPlease install Python first.");
+//                     exit();
+//                 }
+//             });
+//         }
+//     });
 
-    setupProcessHandlers(pythonProcess);
-}
+//     setupProcessHandlers(pythonProcess);
+// }
 
-/**
- * Setup the event handlers for the Python process
- * @param {ChildProcess} pythonProcess - The Python process
- * @returns None
- */
-function setupProcessHandlers(pythonProcess) {
-    pythonProcess.stdout.on("data", (data) => {
-        console.log("Done Scrapping");
-    });
+// /**
+//  * Setup the event handlers for the Python process
+//  * @param {ChildProcess} pythonProcess - The Python process
+//  * @returns None
+//  */
+// function setupProcessHandlers(pythonProcess) {
+//     pythonProcess.stdout.on("data", (data) => {
+//         console.log("Done Scrapping");
+//     });
 
-    pythonProcess.stderr.on("data", (data) => {
-        console.error(`Python script error: ${data}`);
-    });
-}
+//     pythonProcess.stderr.on("data", (data) => {
+//         console.error(`Python script error: ${data}`);
+//     });
+// }
 
 const student_router = require("./routes/student");
 const course_router = require("./routes/course");
@@ -90,7 +90,7 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-runScrapper();
+// runScrapper();
 
 // const courseData = require('./course.json');
 // const courses = courseData.courses;
