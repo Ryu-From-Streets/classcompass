d6b90dbc41eb3293afd4408e47989f64063438a0
Pratim Sadhu <pratimsadhu@icloud.com>
2024-04-26 15:28:01 -0400

    Fn definition

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
+/**
+ * This function connects to the MongoDB database using the provided URL.
+ * @param {*} url The MongoDB URL
+ * @returns The connection to the MongoDB database
+ */
 async function connectMongoDB(url) {
-    return mongoose.connect(url)
+    return mongoose.connect(url);
 }
 
 module.exports = { connectMongoDB };
