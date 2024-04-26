70c973de2781acdb4a1e4deb516586373de8487b
Pratim Sadhu <pratimsadhu@icloud.com>
2024-04-25 23:07:13 -0400

    Auth generate and verify

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,36 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config()
+require("dotenv").config();
 
 /**
- * This function verifies the token of the user 
+ * This function generates a token for the user
+ * @param {*} user The user of the platform
+ * @returns The token of the user
+ */
+async function generateToken(user) {
+    const payload = {
+        userID: user._id,
+        role: user.role,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+    });
+    return token;
+}
+
+/**
+ * This function verifies the token of the user
  * @param {*} req The request object
  * @param {*} res The response object
  * @param {*} next The next middleware function
- * @returns The response object with the message if the token is invalid 
+ * @returns The response object with the message if the token is invalid
  * @returns The next middleware function if the token is valid
  */
-function verifyUserToken(req, res, next) {
-    const token = req.header("Authorization");
+function verifyToken(req, res, next) {
+    const token = req.header("Authorization").replace("Bearer ", "");
     if (!token) {
-        return res.status(401).json({ message: "Access denied" });
+        return res
+            .status(401)
+            .json({ message: "Access denied, no token provided." });
     }
 
     try {
@@ -26,21 +44,37 @@ function verifyUserToken(req, res, next) {
 
 /**
  * This function verifies the token of the user and checks if the user is an admin
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- * @returns 
+ * @param {*} req The request object
+ * @param {*} res The response object
+ * @param {*} next The next middleware function
+ * @returns The response object with the message if the user is not an user
  */
 async function isUser(req, res, next) {
-    if (req.user.role !== "user") {
-        return res.status(403).json({ message: "Access denied" });
+    if (req.user.role !== "student") {
+        return res
+            .status(403)
+            .json({ message: "Access denied, not a student user." });
     }
     next();
 }
 
+/**
+ * The function verifies the token of the user and checks if the user is an admin
+ * @param {*} req The request object
+ * @param {*} res The response object
+ * @param {*} next The next middleware function
+ * @returns The response object with the message if the user is not an admin
+ */
 async function isAdmin(req, res, next) {
     if (req.user.role !== "admin") {
-        return res.status(403).json({ message: "Access denied" });
+        return res.status(403).json({ message: "Access denied, not a admin." });
     }
     next();
 }
+
+module.exports = {
+    generateToken,
+    verifyToken,
+    isUser,
+    isAdmin,
+};
