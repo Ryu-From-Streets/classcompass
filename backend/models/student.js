e80bdd62677691639171531c3c40c4eacd411e6c
Pratim Sadhu <pratimsadhu@icloud.com>
2024-04-27 17:29:31 -0400

    Add last viewed courses

--- backend/models/student.js
+++ backend/models/student.js
@@ -10,7 +10,9 @@ const mongoose = require("mongoose");
  * @property {Number} credits - The number of credits the student has completed
  * @property {Array[string]} courses_taken - The list of courses the student has taken
  * @property {String} advisor - The email of the advisor assigned to the student
+ * @property {Array[string]} last_viewed_courses - The list of courses the student has viewed
  * @property {String} password - The password of the student
+ * @property {String} role - The role of the student
  * @property {Date} createdAt - The timestamp of the creation of the student
  * @property {Date} updatedAt - The timestamp of the last update of the student
  */
@@ -45,10 +47,15 @@ const studentSchema = new mongoose.Schema(
         courses_taken: {
             type: [String],
             default: [],
+            length: 10,
         },
         advisor: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Advisor",
+        },
+        last_viewed_courses: {
+            type: [String],
+            default: [],
         },
         password: {
             type: String,
