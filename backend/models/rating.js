5a71a8835603b8efe11abcdc2b6d4778ba3a6ae6
Pratim Sadhu <pratimsadhu@icloud.com>
2024-05-01 15:01:20 -0400

    Rating Schema changed

--- backend/models/rating.js
+++ backend/models/rating.js
@@ -4,16 +4,19 @@ const ratingSchema = new mongoose.Schema({
     studentID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Student",
+        required: true,
     },
     courseID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Course",
+        required: true,
     },
     value: {
         type: Number,
         required: true,
         min: 1,
         max: 5,
+        required: true,
     },
     comment: {
         type: String,
