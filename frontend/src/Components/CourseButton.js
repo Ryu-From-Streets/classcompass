74d6cae4509f009d92626ff5854f606e58922b59
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-04-30 21:25:39 -0400

    Changed button to display course code as type and number
    Change how the course button displays the course code.
    Previously it would display it as it is formatted in the database (eg. COMPSCI220)
    
    But now course button calls the splitCourseCode function that splits the code into two tokens; one being the course type (eg. COMPSCI) and the other being course number (eg. 220).
    
    This way the course code text is a bit more dynamic, able to wrap around if it is too long, so that course button can be resized easier with less styling issues.

--- frontend/src/Components/CourseButton.js
+++ frontend/src/Components/CourseButton.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-
+import { splitCourseCode } from "../Utils/format_strings";
 import "./CourseButton.css";
+
 
 /* Course Button and CourseInfoPopup adapted from https://www.youtube.com/watch?v=i8fAO_zyFAM */
 
@@ -10,13 +11,17 @@ import CourseInfoPopup from "./CourseInfoPopup";
 const CourseButton = ({ course }) => {
     const [popupVisibility, setPopupVisibility] = useState(false);
 
+    // gets course code as array where first item is the course type (ex: "COMPSCI")
+    // and the second item is the couse number (ex: "220")
+    let courseTokens = splitCourseCode(course.code);
+
     return (
         <>
             <button 
                 className="CourseButton"
                 onClick={() => setPopupVisibility(true)}
             >
-                <p className="class-code">{ course.code }</p>
+                <p className="class-code">{ courseTokens[0] + " " + courseTokens[1] }</p>
                 <p className="class-name">{ course.name }</p>
             </button>
 
