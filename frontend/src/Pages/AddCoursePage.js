21e54a2ba93bf805991eae720919953bb8d4d419
Ryu-From-Streets <mullinsryujin@gmail.com>
2024-05-07 23:24:01 -0400

    Created Edit and Add Course Pages
    created edit and add course pages for future functionality to be implemented that allows admin accounts to edit and add courses.
    
    These pages are currently mostly empty and have no functionality, but exist for future use and implementation.

---
+++ frontend/src/Pages/AddCoursePage.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+
+
+const AddCoursePage = ( {user} ) => {
+
+    useEffect(() => {
+
+    });
+
+    return (
+        <div className="AddCoursePage">
+            <Link className="back-link" to="/">&lt; Back to Search</Link>
+            <h2>Add Course Page Under Construction</h2>
+        </div>
+    );
+
+}
+
+export default AddCoursePage;
